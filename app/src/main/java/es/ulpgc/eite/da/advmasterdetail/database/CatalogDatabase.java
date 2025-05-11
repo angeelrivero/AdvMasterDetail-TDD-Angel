3958a96cdedc0ea75af441f4c2305e0c2d43495b
Angel <angel.rivero104@alu.ulpgc.es>
2025-05-11 14:32:57 +0200

    Actualizar AndroidManifest para establecer MoviesListActivity como pantalla principal y eliminar CategoryListActivity

--- app/src/main/java/es/ulpgc/eite/da/advmasterdetail/database/CatalogDatabase.java
+++ app/src/main/java/es/ulpgc/eite/da/advmasterdetail/database/CatalogDatabase.java
@@ -6,10 +6,9 @@ import androidx.room.RoomDatabase;
 import es.ulpgc.eite.da.advmasterdetail.data.MovieItem;
 
 
-@Database(entities = {CategoryItem.class, MovieItem.class}, version = 1)
+@Database(entities = {MovieItem.class}, version = 1)
 public abstract class CatalogDatabase extends RoomDatabase {
 
-  public abstract CategoryDao categoryDao();
   public abstract MovieDao movieDao();
 
 
